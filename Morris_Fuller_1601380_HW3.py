1f0d84d42be16118b1b4bf71cd63cf58d11aa15a
Morris <morrisjfuller@gmail.com>
2019-04-28 10:29:50 -0700

    all finished except main() and comments

--- Morris_Fuller_1601380_HW3.py
+++ Morris_Fuller_1601380_HW3.py
@@ -1,3 +1,21 @@
+def positionQueries(data, order):
+    if not type(data) or type(order) == "list":
+        print("bad input: type")
+        return
+    elif len(data) == 0:
+        print("bad input: zero")
+        return
+    elif len(order) == 0:
+        print("bad input: zero")
+    elif len(order) > len(data):
+        print("bad input: length")
+        return
+    output = []
+    for i in order:
+        output.append(data[i-1])
+    print(output)
+
+
 def TheRoundTrip(dirs):
     valid_input = ["U", "D", "L", "R"]
     vertical = 0
@@ -27,7 +45,41 @@ def TheRoundTrip(dirs):
             print(False)
 
 
-TheRoundTrip(["U", "U", "R", "L", "D"])
+def isSecretKey(key_ring, secret_key):
+    count = 0
+    key_ring2 = key_ring
+    for i in key_ring:
+        for j in key_ring2:
+            if j != i and i + j == secret_key:
+                count += 1
+    if count > 0:
+        print(True)
+        return
+    else:
+        print(False)
+        return
 
 
+def isAlienWord(word):
+    if len(word) < 1:
+        return True
+    else:
+        if word[0] == word[-1]:
+            return isAlienWord(word[1:-1])
+        else:
+            return False
 
+
+def isAlienLanguage(sentence):
+    count = 0
+    sentence = sentence.split(" ")
+    for word in sentence:
+        if isAlienWord(word):
+            count += 1
+    if count == len(sentence):
+        print(True)
+    else:
+        print(False)
+
+
+isAlienLanguage("a ma")
